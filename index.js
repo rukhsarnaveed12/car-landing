2da8d4476975a6fca80402a8188572dc21088c90
Rukhsar Naveed <mk8076750@gmail.com>
2024-12-09 03:50:08 -0800

    initialize

---
+++ index.js
@@ -0,0 +1,14 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+function handleClick(){
+    alert("click the button and enjoy shopping")
+}
+
+function handle(){
+    alert("Click the button and From Company Contact Us")
+}
